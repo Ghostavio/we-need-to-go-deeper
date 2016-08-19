dcf725528bcce18c47066bc578b9bec253ea09a8
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2016-08-19 17:59:47 +0200

    fixed null problem

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const path = require('path');
 const player = require('play-sound')(opts = {});
 const termImg = require('term-img2');
 
-player.play(path.resolve(__dirname, './mp3/inceptionbutton.mp3'), err => console.log(err));
+player.play(path.resolve(__dirname, './mp3/inceptionbutton.mp3'), err => { if(err) console.log(err); });
 termImg(path.resolve(__dirname, './img/we-need-to-go-deeper.jpg'), {log: true, fallback: () => console.log('We need to go deeper.')});
