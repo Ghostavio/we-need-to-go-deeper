eb28cefce0e19873d1abe1cc89c29f2eafcff60c
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2016-08-19 17:02:12 +0200

    oops, wrong folder 🔥

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const player = require('play-sound')(opts = {});
 const termImg = require('term-img2');
 
-player.play('mp3/inceptionbutton.mp3', function(err){});
-termImg('img/we-need-to-go-deeper.jpg', {log: true});
+player.play('./mp3/inceptionbutton.mp3', function(err){});
+termImg('./img/we-need-to-go-deeper.jpg', {log: true});
